acb6a7a7bb72876a903cd755ad2ed210f6683029
Emanuel Pereira <emanuelcdpr@gmail.com>
2022-06-09 06:41:51 -0300

    feat: ensure SignupController return an error if email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpController {
   handle (httpReqest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpReqest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpReqest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
