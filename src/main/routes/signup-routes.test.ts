4f946878c790c83c21026b607b673e70a1245025
Emanuel Pereira <emanuelcdpr@gmail.com>
2022-07-15 19:21:39 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    app.post('/api/signup', (req, res) => res.send())
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Emanuel Pereira',
+        email: 'emanuelcdpr@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
