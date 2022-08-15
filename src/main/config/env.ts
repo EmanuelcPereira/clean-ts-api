83edc77e1bbfb2a6447e79bef69043224731dd37
Emanuel Pereira <emanuelcdpr@gmail.com>
2022-08-15 17:09:05 -0300

    refactor: remove mongo uri

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://clean-ts-api:KmCUOLyNxg9MbAlc@cluster0.lkslpja.mongodb.net/?retryWrites=true&w=majority',
-  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 5050
 }
