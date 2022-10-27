d44655e72a3eed832277db9bbb8ea15017fffcc6
Emanuel Pereira <emanuelcdpr@gmail.com>
2022-10-27 07:02:42 -0300

    test: ensure DbLoadAccountByToken returns null if decrypter returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -30,8 +30,17 @@ describe('DbLoadAccountByToken UseCase', () => {
   test('should call Decrypter with correct values', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    await sut.load('any_token')
+    await sut.load('any_token', 'any_role')
 
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+
+
+  test('should return null Decrypter returns null', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockResolvedValueOnce(null)
+    const account = await sut.load('any_token', 'any_role')
+
+    expect(account).toBeNull()
   })
 })
--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,12 +1,11 @@
-import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token';
-import { Decrypter } from '../../protocols/criptography/decrypter';
-import { AccountModel } from '../add-account/db-add-accout-protocols';
+import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
+import { Decrypter } from '../../protocols/criptography/decrypter'
+import { AccountModel } from '../add-account/db-add-accout-protocols'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
-  constructor (private readonly decrypter: Decrypter){}
+  constructor(private readonly decrypter: Decrypter) {}
   async load(accessToken: string, role?: string): Promise<AccountModel> {
     await this.decrypter.decrypt(accessToken)
     return Promise.resolve(null)
   }
-
 }
