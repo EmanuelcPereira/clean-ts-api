1c4b6535b9f67131200d487ddec7d1b339a4cdd3
Emanuel Pereira <emanuelcdpr@gmail.com>
2022-10-27 08:00:39 -0300

    test: ensure JwtAdapter throw if verify throws

--- src/infra/criptography/jwt-adapter/jwd-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwd-adapter.spec.ts
@@ -58,5 +58,15 @@ describe('Jwt Adapter', () => {
 
       expect(value).toBe('any_value')
     })
+
+    test('should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any_id')
+
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
